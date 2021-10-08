b666b3ea978f01860ddef7228b6e07999884a14c
Radar <40074095+RadarFolf@users.noreply.github.com>
2021-10-08 11:23:33 -0400

    Update work.js

--- commands/Economy/work.js
+++ commands/Economy/work.js
@@ -11,7 +11,7 @@
 //                                         \______/       
 // © 2019 Casey Media, L.L.C. in partnership with Razor Köpfe GmbH (RazoringMinds GmbH).
 // All bot code or snippets is © 2019 Casey Maxwell AG. All Rights Reserved.
-// RazoringMinds is created by Casey Maxwell (Ca$ey#0001). View the license!
+// RazoringMinds is created by Casey Maxwell (Radar#0001). View the license!
 
 const {MessageEmbed} = require('discord.js');
 const Endb = require('endb');
@@ -66,6 +66,7 @@ module.exports.commandrecent = {
 
 module.exports.help = {
     name: "work",
+    aliases: [],
     description: "Work for money.",
     usage: "work",
     type: "Utility"
