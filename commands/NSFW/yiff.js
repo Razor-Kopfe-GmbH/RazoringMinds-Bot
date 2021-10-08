1b0ad585d93cac0cf74495718e40ce346dc95d32
Radar <40074095+RadarFolf@users.noreply.github.com>
2021-10-08 11:21:53 -0400

    Update yiff.js

--- commands/NSFW/yiff.js
+++ commands/NSFW/yiff.js
@@ -11,37 +11,55 @@
 //                                         \______/       
 // © 2019 Casey Media, L.L.C. in partnership with Razor Köpfe GmbH (RazoringMinds GmbH).
 // All bot code or snippets is © 2019 Casey Maxwell AG. All Rights Reserved.
-// RazoringMinds is created by Casey Maxwell (Ca$ey#0001). View the license!
+// RazoringMinds is created by Casey Maxwell (Radar#0001). View the license!
 
-const superagent = require('superagent');
 const {MessageEmbed} = require('discord.js');
 const lang = require("../../lang.json");
-const FurryBotAPI = require("furrybotapi");
-const fb = new FurryBotAPI();
+const redditimage = require('reddit.images');
+const subredditCategory = [
+    "yiff"
+]
 
 module.exports.run = async (bot, message, args) => {
     if (message.channel.nsfw === true) {
-        const img = await fb.furry.yiff.gay("json", 1);
-        const fourkembed = new MessageEmbed()
-            .setColor(0x00008b)
-            .setTitle("Click here to get image")
-            .setImage(img.url)
-            .setFooter(`Bot by Ca$ey#0001 • Requested by ${message.member.user.username}`, bot.user.avatarURL)
-            .setTimestamp();
-        fourkembed.url = fourkembed.image.url
-        message.channel.send(fourkembed)
+        let subreddit = subredditCategory[Math.floor(Math.random() * subredditCategory.length)];
+        try {
+            await redditimage.fetch({
+                type: "custom",
+                total: 1,
+                subreddit: [subreddit],
+            }).then((result) => {
+                    const embodpa = new MessageEmbed()
+                    .setColor(0x00008B)
+                    .setTitle(`${result[0].title}`)
+                    .setURL(`${result[0].postLink}`)
+                    .setImage(`${result[0].image}`)
+                    .setTimestamp()
+                    .setFooter(`Bot by Radar#0001 • Upvotes: ${result[0].upvotes} Downvotes: ${result[0].downvotes}`, bot.user.avatarURL);
+                message.channel.send(embodpa)  
+            });
+        } catch(err) {
+                const richnewembedI = new MessageEmbed()
+                .setColor("0x00008B")
+                .setTitle("Unknown unexplained error.")
+                .addField(`Error: ${err}`, "Error UNKNOWN-ERR-50147")
+                .addField("If you believe this is in error, let us know.", `Please join our support server ${lang["en_US"].support_server}`)
+            message.channel.send(richnewembedI)
+          }
+
     } else {
-        const richnewembed = new MessageEmbed()
-            .setColor("0x00008B")
-            .setTitle("This is not an NSFW channel.")
-            .addField("Please use the .setnsfw command in order to set a channel as NSFW", "Error NON-NSFW-50157")
-            .addField("If you believe this is in error, let us know.", `Please join our support server ${lang["en_US"].support_server}`)
-        message.channel.send(richnewembed)
-    }
+            const richnewembed = new MessageEmbed()
+                .setColor("0x00008B")
+                .setTitle("This is not an NSFW channel.")
+                .addField("Please use the .setnsfw command in order to set a channel as NSFW", "Error NON-NSFW-50157")
+                .addField("If you believe this is in error, let us know.", `Please join our support server ${lang["en_US"].support_server}`)
+            message.channel.send(richnewembed)
+          }
 }
 
 module.exports.help = {
     name: "yiff",
+    aliases: [],
     description: "18+ Command.",
     usage: "yiff",
     type: "Utility"
